962a8b0efd014dddcbd2c61a235ca95dba4c3a54
bleizing <bleizing97@gmail.com>
2021-06-02 13:33:55 +0700

    Init Commit

---
+++ src/main/java/com/infosys/sejutakebaikancms/SejutaKebaikanCmsApplication.java
@@ -0,0 +1,13 @@
+package com.infosys.sejutakebaikancms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SejutaKebaikanCmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SejutaKebaikanCmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infosys/sejutakebaikancms/SejutaKebaikanCmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infosys.sejutakebaikancms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SejutaKebaikanCmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
